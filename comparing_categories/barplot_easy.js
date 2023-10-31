77d16f7e17f7cb703c7283ecc096fd5d0392ddef
Aliaksandr Shcharbitski <aliaksandr.shcharbitski@gmail.com>
2023-10-31 15:42:57 +0300

    Add heatmap and fix namespaces

--- comparing_categories/barplot_easy.js
+++ comparing_categories/barplot_easy.js
@@ -1,168 +1,174 @@
-const size = 20;
-const key = "common_name";
 
-const width = window.screen.width * 0.8;
-const height = 480;
-const margin = { t: 10, b: 30, l: 120, r: 10 };
+function run_barplot(divname) {
 
-var sort_order = "tree_count"
-var selectedFile = "../data_raw/Albuquerque_Final_2022-06-18.csv"
+  const size = 20;
+  const key = "common_name";
 
-const svg = d3.select('div#barContainer')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height)
-  .style('border', 'solid 1px #222');
+  const width = window.screen.width * 0.6;
+  const height = 480;
+  const margin = { t: 10, b: 30, l: 120, r: 10 };
 
-const chart = svg.append('g')
-  .classed('chart', true)
-  .attr('transform', `translate(${margin.l},${margin.t})`);
+  var sort_order = "tree_count"
+  var selectedFile = "../data_raw/Albuquerque_Final_2022-06-18.csv"
 
-const xAxis = svg.append('g')
-  .classed('axis', true)
-  .attr('transform', `translate(${margin.l},${height - margin.b})`);
+  const svg = d3.select(`div#${divname}`)
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .style('border', 'solid 1px #222');
 
-const yAxis = svg.append('g')
-  .classed('axis', true)
-  .attr('transform', `translate(${margin.l},${margin.t})`);
+  const chart = svg.append('g')
+    .classed('chart', true)
+    .attr('transform', `translate(${margin.l},${margin.t})`);
 
-document.getElementById("fileSelect").addEventListener("change", function (e) {
-  selectedFile = e.target.value;
-  if (selectedFile) {
-    console.log(selectedFile);
-    updateData(selectedFile);
-  }
-});
+  const xAxis = svg.append('g')
+    .classed('axis', true)
+    .attr('transform', `translate(${margin.l},${height - margin.b})`);
 
-document.querySelectorAll('input[type="radio"][name="sortorder"]').forEach((radioButton) => {
-    radioButton.addEventListener('change', (event) => {
-      if (event.target.checked) {
-        console.log(`Selected: ${event.target.value}`);
-        sort_order = event.target.value;
-        updateData(selectedFile)
-      }
-    });
+  const yAxis = svg.append('g')
+    .classed('axis', true)
+    .attr('transform', `translate(${margin.l},${margin.t})`);
+
+  document.getElementById("fileSelect").addEventListener("change", function (e) {
+    selectedFile = e.target.value;
+    if (selectedFile) {
+      console.log(selectedFile);
+      updateData(selectedFile);
+    }
   });
 
-
-function updateData(csvData) {
-  d3.csv(csvData).then(function (data) {
-    const groupedData = d3.group(data, (d) => d[key]);
-    const dataset = Array.from(groupedData.keys())
-      .sort((a, b) => groupedData.get(b).length - groupedData.get(a).length)
-      .filter((x) => x != 'NA')
-      .slice(0, size)
-      .map((el, index) => {
-        const heights = groupedData.get(el).map((tree) => tree.height_M).filter((height) => height != "NA");
-        const properties = {
-          "key": el.slice(0,20),
-          "value": groupedData.get(el).length,
-          "rank": index,
-          "height": heights.length > 0 ? Math.round(d3.sum(heights) / heights.length * 10) / 10 : "no data",
-        };
-        return properties;})
-      .sort((a, b) => {
-        if (sort_order == "tree_count") {
-            return b.value - a.value;
-        } else if (sort_order == "tree_name") {
-            return a.key.localeCompare(b.key);
+  document.querySelectorAll('input[type="radio"][name="sortorder"]').forEach((radioButton) => {
+      radioButton.addEventListener('change', (event) => {
+        if (event.target.checked) {
+          console.log(`Selected: ${event.target.value}`);
+          sort_order = event.target.value;
+          updateData(selectedFile)
         }
       });
+    });
 
-    console.log(dataset);
 
-    const d = 500;
-    const d_short = 300;
-    const tRemove = d3.transition().duration(d);
-    const tPosition = d3.transition().duration(d).delay(d);
-    const tSize = d3.transition().duration(d).delay(d * 2);
+  function updateData(csvData) {
+    d3.csv(csvData).then(function (data) {
+      const groupedData = d3.group(data, (d) => d[key]);
+      const dataset = Array.from(groupedData.keys())
+        .sort((a, b) => groupedData.get(b).length - groupedData.get(a).length)
+        .filter((x) => x != 'NA')
+        .slice(0, size)
+        .map((el, index) => {
+          const heights = groupedData.get(el).map((tree) => tree.height_M).filter((height) => height != "NA");
+          const properties = {
+            "key": el.slice(0,20),
+            "value": groupedData.get(el).length,
+            "rank": index,
+            "height": heights.length > 0 ? Math.round(d3.sum(heights) / heights.length * 10) / 10 : "no data",
+          };
+          return properties;})
+        .sort((a, b) => {
+          if (sort_order == "tree_count") {
+              return b.value - a.value;
+          } else if (sort_order == "tree_name") {
+              return a.key.localeCompare(b.key);
+          }
+        });
 
-    const xScale = d3.scaleLinear()
-      .domain([0, d3.max(dataset, (d) => d.value)])
-      .range([0, width - margin.l - margin.r]);
+      console.log(dataset);
 
-    const yScale = d3.scaleBand()
-      .domain(dataset.map((d) => d.key))
-      .range([0, height - margin.t - margin.b])
-      .padding(0.2);
+      const d = 500;
+      const d_short = 300;
+      const tRemove = d3.transition().duration(d);
+      const tPosition = d3.transition().duration(d).delay(d);
+      const tSize = d3.transition().duration(d).delay(d * 2);
 
-    d3.axisBottom(xScale)(xAxis.transition(tSize));
-    d3.axisLeft(yScale)(yAxis.transition(tPosition));
+      const xScale = d3.scaleLinear()
+        .domain([0, d3.max(dataset, (d) => d.value)])
+        .range([0, width - margin.l - margin.r]);
 
-    let bars = chart.selectAll('rect.bar');
+      const yScale = d3.scaleBand()
+        .domain(dataset.map((d) => d.key))
+        .range([0, height - margin.t - margin.b])
+        .padding(0.2);
 
-    bars = bars.data(dataset, (d) => d.key);
+      d3.axisBottom(xScale)(xAxis.transition(tSize));
+      d3.axisLeft(yScale)(yAxis.transition(tPosition));
 
-    bars.exit()
-      .classed('obs', true)
-      .transition(tRemove)
-      .attr('width', 0)
-      .remove();
+      let bars = chart.selectAll('rect.bar');
 
-    const tooltip = d3.select("body")
-      .append("div")
-      .classed("tooltip", true);
+      bars = bars.data(dataset, (d) => d.key);
 
-    const barsEnter = bars.enter().append('rect')
-      .classed('bar new', true)
-      .attr('x', xScale(0))
+      bars.exit()
+        .classed('obs', true)
+        .transition(tRemove)
+        .attr('width', 0)
+        .remove();
+
+      const tooltip = d3.select("body")
+        .append("div")
+        .classed("tooltip", true);
+
+      const barsEnter = bars.enter().append('rect')
+        .classed('bar new', true)
+        .attr('x', xScale(0))
+        .on('mouseover', function (e, d) {
+          d3.selectAll('rect.bar').classed('unhighlight', true);
+          d3.select(this).classed('unhighlight', false);
+          d3.select(this).classed('highlight', true);
+          tooltip.text(`${dataset[d].key} trees: ${dataset[d].value};\n\tavg height: ${dataset[d].height}`);
+          return tooltip.style("z-index", "10")
+            .transition().duration(d_short)
+            .style("opacity", 1);
+        })
+        .on("mousemove", function () {
+          return tooltip.style("top", (d3.event.pageY - 10) + "px")
+            .style("left", (d3.event.pageX + 10) + "px");
+        })
+        .on('mouseout', function (e, d) {
+          d3.selectAll('rect.bar').classed('unhighlight', false);
+          d3.select(this).classed('highlight', false);
+          return tooltip.transition().duration(d_short)
+            .style("opacity", 0)
+            .style("z-index", "-10");
+        });
+
+        
+      bars.classed('new', false)
       .on('mouseover', function (e, d) {
-        d3.selectAll('rect.bar').classed('unhighlight', true);
-        d3.select(this).classed('unhighlight', false);
-        d3.select(this).classed('highlight', true);
-        tooltip.text(`${dataset[d].key} trees: ${dataset[d].value};\n\tavg height: ${dataset[d].height}`);
-        return tooltip.style("z-index", "10")
-          .transition().duration(d_short)
-          .style("opacity", 1);
-      })
-      .on("mousemove", function () {
-        return tooltip.style("top", (d3.event.pageY - 10) + "px")
-          .style("left", (d3.event.pageX + 10) + "px");
-      })
-      .on('mouseout', function (e, d) {
-        d3.selectAll('rect.bar').classed('unhighlight', false);
-        d3.select(this).classed('highlight', false);
-        return tooltip.transition().duration(d_short)
-          .style("opacity", 0)
-          .style("z-index", "-10");
-      });
+          d3.selectAll('rect.bar').classed('unhighlight', true);
+          d3.select(this).classed('unhighlight', false);
+          d3.select(this).classed('highlight', true);
+          tooltip.text(`${dataset[d].key} trees: ${dataset[d].value};\n\tavg height: ${dataset[d].height}`);
+          return tooltip.style("z-index", "10")
+            .transition().duration(d_short)
+            .style("opacity", 1);
+        })
+        .on("mousemove", function () {
+          return tooltip.style("top", (d3.event.pageY - 10) + "px")
+            .style("left", (d3.event.pageX + 10) + "px");
+        })
+        .on('mouseout', function (e, d) {
+          d3.selectAll('rect.bar').classed('unhighlight', false);
+          d3.select(this).classed('highlight', false);
+          return tooltip.transition().duration(d_short)
+            .style("opacity", 0)
+            .style("z-index", "-10");
+        });
 
-      
-    bars.classed('new', false)
-    .on('mouseover', function (e, d) {
-        d3.selectAll('rect.bar').classed('unhighlight', true);
-        d3.select(this).classed('unhighlight', false);
-        d3.select(this).classed('highlight', true);
-        tooltip.text(`${dataset[d].key} trees: ${dataset[d].value};\n\tavg height: ${dataset[d].height}`);
-        return tooltip.style("z-index", "10")
-          .transition().duration(d_short)
-          .style("opacity", 1);
-      })
-      .on("mousemove", function () {
-        return tooltip.style("top", (d3.event.pageY - 10) + "px")
-          .style("left", (d3.event.pageX + 10) + "px");
-      })
-      .on('mouseout', function (e, d) {
-        d3.selectAll('rect.bar').classed('unhighlight', false);
-        d3.select(this).classed('highlight', false);
-        return tooltip.transition().duration(d_short)
-          .style("opacity", 0)
-          .style("z-index", "-10");
-      });
+        
+      bars.merge(barsEnter)
+        .transition(tPosition)
+        .attr('y', (d) => yScale(d.key))
+        .attr('height', yScale.bandwidth())
+        .transition(tSize)
+        .attr('width', (d) => xScale(d.value));
 
-      
-    bars.merge(barsEnter)
-      .transition(tPosition)
-      .attr('y', (d) => yScale(d.key))
-      .attr('height', yScale.bandwidth())
-      .transition(tSize)
-      .attr('width', (d) => xScale(d.value));
+        
+      setTimeout(() => {
+        bars.merge(barsEnter).classed('new', false);
+      }, d * 4);
+    });
+  }
 
-      
-    setTimeout(() => {
-      bars.merge(barsEnter).classed('new', false);
-    }, d * 4);
-  });
+  updateData(selectedFile);
 }
 
-updateData(selectedFile);
+run_barplot("barContainer")
---
+++ comparing_categories/heatmap.js
@@ -0,0 +1,83 @@
+function run_heatmap(div_name) {
+    var margin = {top: 20, right: 20, bottom: 30, left: 100},
+    width = window.screen.width * 0.4 - margin.left - margin.right,
+    height = window.screen.width * 0.4 - margin.top - margin.bottom;
+    const d_short = 50;
+
+    var svg = d3.select(`#${div_name}`)
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.csv("data/california_top_five_trees.csv").then(function(data) {
+
+        console.log(data);
+
+        var myGroups = data.map(el => el.City);
+
+        var myVars = Object.keys(data[0]).filter(el => el != "City" && el != "Other")
+        
+
+        var x = d3.scaleBand()
+            .range([ 0, width ])
+            .domain(myGroups)
+            .padding(0.01);
+        
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x))
+
+        var y = d3.scaleBand()
+            .range([ height, 0 ])
+            .domain(myVars)
+            .padding(0.01);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+
+        let line_data = data.flatMap(row => myVars.map(el => {return {"key": row.City, "var": el, "val": +row[el]}}));
+
+        console.log(line_data);
+        console.log(line_data.reduce((prev, curr) => prev.val > curr.val ? prev : curr))
+
+        var myColor = d3.scaleLinear()
+        .range(["white", "#69b3a2"])
+        .domain([0, line_data.reduce((prev, curr) => prev.val > curr.val ? prev : curr).val])
+
+        const tooltip = d3.select("body")
+            .append("div")
+            .classed("tooltip", true);
+
+        svg.selectAll()
+            .data(line_data, d => d)
+            .enter()
+            .append("rect")
+            .attr("x", function(d) { return x(d.key)})
+            .attr("y", function(d) { return y(d.var) })
+            .attr("width", x.bandwidth() )
+            .attr("height", y.bandwidth() )
+            .style("fill", function(d) { return myColor(d.val)} )
+            .on('mouseover', function (e, d) {
+                d3.select(this).classed('highlight', true);
+                tooltip.text(`${line_data[d].var} trees in ${line_data[d].key}: ${line_data[d].val}`);
+                return tooltip.style("z-index", "10")
+                  .transition().duration(d_short)
+                  .style("opacity", 1);
+              })
+              .on("mousemove", function () {
+                return tooltip.style("top", (d3.event.pageY - 10) + "px")
+                  .style("left", (d3.event.pageX + 10) + "px");
+              })
+              .on('mouseout', function (e, d) {
+                d3.select(this).classed('highlight', false);
+                return tooltip.transition().duration(d_short)
+                  .style("opacity", 0)
+                  .style("z-index", "-10");
+              });
+    })
+}
+
+run_heatmap("heatmapContainer")
